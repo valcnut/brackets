d1bab29c8756c08df02695560b6bbb525ebc162c
valcnut <kzmienko@yandex.ru>
2023-02-23 15:17:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+for (let i = 0; i < bracketsConfig.length;)
+  if (!str.includes(bracketsConfig[i].join(''))) {
+    i++;
+  } else {
+    str = str.replace(bracketsConfig[i].join(''), '');
+    i = 0;
+  }
+  if (str.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
